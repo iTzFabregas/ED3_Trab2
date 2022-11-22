f7154c4ad900f9a96dde74add0e7269057650ebc
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2022-11-21 22:50:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,47 @@
+/*
+Fabricio Sampaio [runcodes] - 12547423 - participacao: 100%
+Pedro Lucas Castro de Andrade - 11212289 - participacao: 100%
+*/
+
+#include <stdio.h>
+
+//#include "command7.h"
+#include "command8.h"
+//#include "command9.h"
+//#include "command10.h"
+#include "funcoesFornecidas.h"
+
+int main(void) {
+    //variaveis p/ entrada do teclado
+    int command;
+    char data_file1[30];
+    char data_file2[30];
+    char index_file[30];
+    int num_suarches;
+
+    //le do teclado qual o comando sera acionado
+    scanf("%d ", &command);
+
+    //aciona a execucao do comando lido
+    switch (command){
+        case 7:
+            scanf("%s %s", data_file1, index_file);
+            /*if(command7(data_file1, index_file)){
+                binarioNaTela(index_file);
+            }*/
+            break;
+        case 8:
+            scanf("%s %s %d", data_file1, index_file, &num_suarches);
+            command8(data_file1, index_file, num_suarches);
+            break;
+        case 9:
+            scanf("%s %s", data_file1, index_file);
+            /*if(command7(file1, file2)){
+                binarioNaTela(file2);
+            }*/
+            break;
+        case 10:
+            
+            break;
+    }
+}
---
+++ print_msg.h
@@ -0,0 +1,7 @@
+#include "register.h"
+
+//mensagem de erro para erro ao tentar abrir um arquivo
+void error_file();
+
+//mensagem de erro para erro p/ registro inexistente
+void error_reg();
