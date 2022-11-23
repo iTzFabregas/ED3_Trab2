8a5dac336bbdacc4b7b5662d6e29292cf8541c7b
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2022-11-23 00:20:09 -0300

    Add files via upload

--- search.h
+++ search.h
@@ -1,6 +1,8 @@
 #ifndef __SEARCH_H__
 #define __SEARCH_H__
 
-void command8(char* data_file, char* index_file, int num_searches);
+void command8(char* data_name, char* index_name, int num_searches);
+
+void command10(char* data_name1, char* data_name2, char* index_name);
 
 #endif
